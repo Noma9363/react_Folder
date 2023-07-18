4241aa1fec8fe2f802a802284fbf47fbd490a319
Noma9363 <noma9636@gmail.com>
2023-07-18 15:42:35 +0900

    movei

---
+++ my-movie-app/src/components/Movie.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
+//ㄴpropTypes 앞에 소문자로 하기 그래야 에러 안남 이유는?
+function Movie({ coverImg, title, summary, genres }) {
+    return (
+        <div>
+            <img src={coverImg} alt={title}/>
+            <h2>
+                <Link to="/movie"> 
+                    {title}
+                </Link>
+            </h2>
+            <p>{summary}</p>
+            <ul>
+                {genres.map((g) => (
+                    <li key={g}>{g}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+//
+Movie.propTypes ={
+    coverImg : propTypes.string.isRequired,
+    title : propTypes.string.isRequired,
+    summary : propTypes.string.isRequired,
+    genres : propTypes.arrayOf(propTypes.string).isRequired,
+};
+
+export default Movie;
